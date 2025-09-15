93eb8fcea16c39ed9de08b62d34daaf853abdb5c
Rantsu\Devaus <mflyktman05@gmail.com>
2025-09-15 12:12:49 +0300

    harjoitus

--- Assets/Scripts/TestingScript.cs
+++ Assets/Scripts/TestingScript.cs
@@ -5,7 +5,7 @@ public class TestingScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Mom said if you die it's my turn
     }
 
     // Update is called once per frame
